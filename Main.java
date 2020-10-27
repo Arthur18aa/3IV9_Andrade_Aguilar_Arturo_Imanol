706889ede93ab8f80939b3d478144af1852251a8
Arthur18aa <aandradea1900@alumno.ipn.mx>
2020-10-27 02:00:13 +0000

    Examen hecho

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.*;
 import java.io.*;
-
+//Se nombran las librerias
 class Main{
 
     public static void main(String[] args) throws 
@@ -21,7 +21,7 @@ class Main{
          int fuerza3 = 55;
          int ataque3 = 14;       
          int ataquesec3 = 23;
-
+      //Se declaran las variables
              
     do{
        System.out.println("Este programa fue elaborado por:");
@@ -38,11 +38,15 @@ class Main{
        System.out.println("1--Charmander");
        System.out.println("2--Bulbasaur");
        System.out.println("3--Squirtle");
-       System.out.println("4--Salir");
+       System.out.println("4--Ir a la Batalla");
+       System.out.println("5--Salir");
        
+       //Se mencionan las opciones que hay y el funcionamiento de cada una
+
        opcion = entrada.nextInt();
     
        switch (opcion) {
+         //Se abren las opciones a escoger
            case 1:
             System.out.println("¡Buenas Elección!, escogiste a Charmander");
             System.out.println("La ID de tu Charmander es: 1");
@@ -57,7 +61,7 @@ class Main{
              ataquesec1 = 20 ;
 
                break;
-
+      //Se dice el contenido de la opcion
            case 2:
              System.out.println("¡Ey, buena decision! Elegiste a Bulbasaur");
              System.out.println("La ID de tu Bulbasaur es: 2");
@@ -69,7 +73,8 @@ class Main{
              fuerza2 = 60;
              ataque2 = 40;
              ataquesec2 = 17;
-              break;    
+              break;  
+      //Se dice el contenido de la opcion
            case 3:
              System.out.println("¡Oleee, que gran decision! Elegiste a Squirtle");
              System.out.println("La ID de tu Squirtle es: 3");
@@ -82,16 +87,70 @@ class Main{
              ataque3 = 14;
              ataquesec3 = 23;
               break; 
+        //Se dice el contenido de la opcion
            case 4:
-                
+                System.out.println("Bienvenido al modo batalla");
+        System.out.println("Elije a a tu pokemon");
+        System.out.println("1--Charmander");
+        System.out.println("2--Bulbasaor");
+        System.out.println("3--squartle");
+        vida1 = entrada.nextInt();
+        vida2 = entrada.nextInt();
+        vida3 = entrada.nextInt();
+        //Se escriben las caractristicas
 
+        if (vida1 == 95){
+            System.out.println("Has elegido Charmander");
+            System.out.println("Tu enemigo sera Bulbasaur");
+            System.out.println("Batalla neutra");
+            
+            System.out.println("Elige tu ataque, (1= Primario, 2= Secundario");
+            ataque1 = entrada.nextInt();
+            //Se abre un if para verificar la caracteristica del ataque y saber si es efectivo o no
+            if (ataque1 == 45 ){
+            System.out.println("Haz atacado a Bulbasaur");
+            }else{
+                System.out.println("El ataque Tormenta de fuego fue efectivo");
+                System.out.println("Bulbasaur esta cansado y ha sido derrotado");
+            }
+        }
+        if (vida2 == 110){
+            System.out.println("Has elegido Squirtle");
+            System.out.println("Tu enemigo sera Charmander");
+            System.out.println("Estas en ventaja");
+            
+            System.out.println("Elige tu ataque, (1= primario, 2= secundario");
+            ataque2 = entrada.nextInt();
+            if (ataque2 == 40){
+            System.out.println("");
+            }else{
+                System.out.println("El ataque chorro de agua realizado fue efectivo y critico");
+                System.out.println("Charmander se ha cansado y ha sido derrotado");
+            }
+        }
+        if (vida3 == 105 ){
+            System.out.println("Has elegido Bulbasaur");
+            System.out.println("Tu enemigo sera Squirtle");
+            System.out.println("Estas en desventaja");
+            
+            System.out.println("Elige tu ataque, (1. Primario, 2. Secundario");
+            ataque3 = entrada.nextInt();
+            if (ataque3 == 14){
+            System.out.println("Haz atacado a Squirtle con fuerza");
+            }else{
+                System.out.println("Haz atacado a Squirtle con latigazo y el ataque fue efectivo");
+                System.out.println("Squirtle se ha cansado y ha sido derrotado");
+            }
+        }
              break;     
            case 5:
             default:
-               break;    
+               break;  
+               //con esta opcion se sale directamente
        } 
+
         }while (opcion != 5); 
-        
+          
 
     } 
 }           
